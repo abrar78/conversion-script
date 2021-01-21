a8f0b53f39029ed3d51273184cda0aa97994d65e
abrar <9529693800aa@gmail.com>
2021-01-21 20:42:57 +0530

    final commit

--- convertapi_conversion_script.py
+++ convertapi_conversion_script.py
@@ -10,10 +10,7 @@
 import convertapi
 
 def convert_convertApi(source_file,target_file):
-    #Pass the parameters with the source file absolute location and the target file absolute location where you want to save the file example:-
-    #source_file='~/test.docx'
-    #target_file='~/converted/test.pdf
-    #note :- send complete file path with the proper extension
+
     is_converted=False
 
     convertapi.api_secret = 'Vm0zyKIXYRRAv1dC' #Your convert api secret key , you will get it from the convertapi acccount , here this key is for testing purpose only
